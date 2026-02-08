e3580864856513bdfbf7aa5937eb9e69f6178b04
JTanner04 <jtanner3@crimson.ua.edu>
2026-02-07 20:51:05 -0600

    Add project files.

---
+++ Spring2026-Project1-jkwest2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Spring2026_Project1_jkwest2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Spring2026-Project1-jkwest2/wwwroot/script.js
@@ -0,0 +1,1 @@
+﻿console.log("Project 1");
